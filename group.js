35e04c9338ae28f97880b4e423be89ca373a742b
harshitsharma88 <harshitsrma88@gmail.com>
2024-05-23 15:48:42 +0530

    favicon

--- group.js
+++ group.js
@@ -11,6 +11,7 @@ app.use(express.static('public'));
 app.use(bodyparser.urlencoded({extended:false}));
 app.use(bodyparser.json());
 app.use(express.json());
+app.use(express.static('public'));
 
 
 /////////---Routes---///////////////
