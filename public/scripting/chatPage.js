015b510ae9960d420fe1f21d1b0fdfd12602ef6e
harshitsharma88 <harshitsrma88@gmail.com>
2024-05-24 10:44:19 +0530

    page

--- public/scripting/chatPage.js
+++ public/scripting/chatPage.js
@@ -1,10 +1,10 @@
 const username= localStorage.getItem("username");
 const token = localStorage.getItem("token");
 
-document.querySelector('.title').textContent="Welcome "+username;
+document.querySelector('#username').textContent="Welcome "+username;
 
 
 document.querySelector('.logout-button').addEventListener('click',()=>{
     localStorage.removeItem('token');
-    location.replace('http://50.16.9.194')
+    location.replace('http://localhost:3000')
 })
