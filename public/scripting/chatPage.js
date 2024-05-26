48f8455befde5754e77f65995055799ae36bbb7c
harshitsharma88 <harshitsrma88@gmail.com>
2024-05-26 08:45:14 +0530

    msg

--- public/scripting/chatPage.js
+++ public/scripting/chatPage.js
@@ -1,10 +1,65 @@
+window.onload=scrollChatSection;
+
 const username= localStorage.getItem("username");
 const token = localStorage.getItem("token");
+const section =document.querySelector('.chat-area');
 
+
+/////-Welcome Text-///////
 document.querySelector('#username').textContent="Welcome "+username;
 
-
-document.querySelector('.logout-button').addEventListener('click',()=>{
+///////-Logout-Button-/////
+document.querySelector('.logout-btn').addEventListener('click',()=>{
     localStorage.removeItem('token');
-    location.replace('http://54.237.202.193')
-})
+    location.replace('http://localhost:3000')
+});
+
+////////---Scroll Chat Section Whenevr new message receives---//////////
+
+function scrollChatSection(){
+    section.scrollTop=section.scrollHeight
+
+}
+
+document.querySelector('.message-form').addEventListener('submit',sendmessage);
+
+function receivemessage(event){
+    event.preventDefault();
+    const message=event.target.message.value;
+
+    const other= document.createElement('div');
+    other.classList='message other'
+
+    const receivername= document.createElement('div');
+    receivername.className='name';
+    receivername.textContent=`${username}`;
+
+    const recivermsg = document.createElement('div');
+    recivermsg.className='text';
+    recivermsg.textContent=`${message}`;
+
+    other.appendChild(receivername);
+    other.appendChild(recivermsg);
+    section.appendChild(other); 
+
+    scrollChatSection();
+}
+
+function sendmessage(event){
+    event.preventDefault();
+    const message=event.target.message.value;
+
+    const sender = document.createElement('div');
+    sender.classList='message sender';
+
+    const text= document.createElement('div');
+    text.classList='text';
+    text.textContent=`${message}`;
+
+    sender.appendChild(text);
+    section.appendChild(sender);
+
+    scrollChatSection();
+
+
+}
